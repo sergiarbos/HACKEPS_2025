4f147b2b1f5c0aaeadf5d9082791d6f10c330318
Arnnau Farràs <arnaufm2@gmail.com>
2025-11-23 08:51:02 +0100

    running app 2

--- HACK_EPS_2025/settings.py
+++ HACK_EPS_2025/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "HACK_EPS_2025" /"static",
 ]
 
 # Default primary key field type
